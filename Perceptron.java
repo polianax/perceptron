8cbb2a06fd4c0777d40429e831d679d89f17baa9
polianax <33838295+polianax@users.noreply.github.com>
2017-11-20 16:13:28 -0200

    Update Perceptron.java

--- Perceptron.java
+++ Perceptron.java
@@ -1,4 +1,3 @@
-
 package perceptron;
 import java.util.Random;
 
@@ -8,60 +7,54 @@ import java.util.Random;
  */
 public class Perceptron {
 
+    double limite;
     double[] pesos;
-    double threshold;
-    public void A(double[][] entradas, int[] saidas, double threshold, double lrate, int ciclos)
+    public void A(double[][] entradas, int[] saidas, double limite, double learnrate, int ciclos)
     {
 
-        this.threshold = threshold;
-        int n = entradas[0].length;
-        int p = saidas.length;
-        pesos = new double[n];
-        Random r = new Random();
+        this.limite = limite;
+        Random r = new Random(); // definido para gerar valores aleatórios
+        int n = entradas[0].length; // n é o número de entradas
+        int p = saidas.length; // p é o numero de saídas
+        pesos = new double[n]; //o vetor peso tera tamanho N que é o numero de entradas
         
-        //aqui inicio os pesos
+        //aqui gero os pesos aleatórios para começar
         for(int i=0;i<n;i++)
         {
-            pesos[i] = r.nextDouble();
+            pesos[i] = r.nextDouble(); //armazeno os valores aleátorios no vetor peso
         }
-
-        for(int i=0;i<ciclos;i++)
+        
+        //
+        for(int i=0;i<ciclos;i++) //para parar quando atingir o numero maximo de ciclos
         {
-            int totalErro = 0;
-            for(int j =0;j<p;j++)
+            int totalErro = 0; //inicia erro igual a 0
+            for(int j =0;j<p;j++) //
             {
-                int saida = Saida(entradas[j]);
-                int erro = saidas[j] - saida;
+                int saida = Saida(entradas[j]); // aqui o vetor saida recebe o retorno da saida
+                int erro = saidas[j] - saida; // aqui comparo com o valor esperado
                 
-                totalErro +=erro;
+                totalErro +=erro; //calculo o novo erro
                
                 for(int k = 0;k<n;k++)
                 {
-                    double delta = lrate * entradas[j][k] * erro;
+                    double delta = learnrate * entradas[j][k] * erro; //cacula delta para 
                     pesos[k] += delta;
-                }
-                
-                
+                }    
             }
-            if(totalErro == 0)
+            if(totalErro == 0) //quando total for zero, para de recalcular os pesos
                 break;
         }
-
     }
-
     public int Saida (double[] entrada)
     {
-        double soma = 0.0;
-        for(int i=0;i<entrada.length;i++)
-        {
-            soma += pesos[i]*entrada[i];
+        double soma = 0.0; //soma inicia com zero
+        for(int i=0;i<entrada.length;i++) //para ao atingir o numero de entradas
+        {  
+            soma += pesos[i]*entrada[i]; // no final deste loop tera realizado somatoria das entradas multiplicadas pelos seus respectivos pesos
         }
-
-        if(soma>threshold)
+        if(soma>limite) //se a somatoria for maior que o limite, entao retorna 1 se não retorna zero, pois é uma função degrau
             return 1;
         else
             return 0;
     }
-
-        
 }
