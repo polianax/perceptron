22b1c2bf084addb3d1eb257ad77202e8e35d669c
polianax <33838295+polianax@users.noreply.github.com>
2017-11-20 16:15:34 -0200

    Update PerceptronAnd.java

--- PerceptronAnd.java
+++ PerceptronAnd.java
@@ -7,14 +7,14 @@ package perceptron;
 public class PerceptronAnd {
     
     public static void main(String[] args) {
-        Perceptron p = new Perceptron();
-        double entradas[][] = {{1,1},{0,1},{1,0},{0,0}};
-        int saidas[] = {0,0,0,1};
+        Perceptron perc = new Perceptron();
+        double entradas[][] = {{1,1},{1,0},{0,1},{0,0}};
+        int saidas[] = {1,0,0,0};
 
-        p.A(entradas, saidas,0.2, 0.1, 200);
-        System.out.println(p.Saida(new double[]{0,0}));
-        System.out.println(p.Saida(new double[]{1,0}));
-        System.out.println(p.Saida(new double[]{0,1}));
-        System.out.println(p.Saida(new double[]{1,1}));
+        perc.A(entradas, saidas,0.1, 0.05, 500);
+        System.out.println(perc.Saida(new double[]{1,1}));
+        System.out.println(perc.Saida(new double[]{1,0}));
+        System.out.println(perc.Saida(new double[]{0,1}));
+        System.out.println(perc.Saida(new double[]{0,0}));
     }
 }
